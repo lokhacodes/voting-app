b53d5df8891f277f35b331c7c1f9463913962456
lokhacodes <sumaiyasonali421@gmail.com>
2026-01-07 20:30:31 +0600

    connect to db

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { PORT, mongoDBURL } from './config.js';
+import { PORT, mongoDBURL } from './.env';
 import mongoose from 'mongoose';
 
 const app = express();
