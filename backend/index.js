3100c664d4588a864c3c7a99d9e39c62316a83a3
lokhacodes <sumaiyasonali421@gmail.com>
2026-01-07 22:14:13 +0600

    save book with mongoose.//route for get 1 book

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
+import { Book } from './models/bookmodel.js';
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
     
@@ -10,10 +12,62 @@ app.get('/', (req, res) => {
 });
 
 
+//route to save new book
+app.post('/books', async(req, res) => {
+    // Logic to add a new book will go here
+    try {
+        if(!req.body.title || !req.body.author || !req.body.publishYear) {
+            return res.status(400).send({
+                message: 'send all required fields: title, author, publishYear'
+            });
+        }
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+        return res.status(201).send(book);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send({ message: error.message });
+    }
+});
+
+//route for get all books 
+app.get('/books', async (req, res)=>{
+    try{
+        const books = await Book.find({});
+        return res.status(200).json({
+            count: books.length,
+            data:books
+        });
+    }catch(error){
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+});
+
+//route for get 1 book
+app.get('/:id', async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const book = await Book.findById(id);
+
+    return response.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 
 
 mongoose
-    .connect(mongoDBURL)
+    .connect(mongoDBURL, { ssl: true, sslValidate: false })
     .then(() => {
         console.log('Connected to MongoDB');
         app.listen(PORT, () => {
